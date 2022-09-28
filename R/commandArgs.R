14320b81fb443235178f82d58627fac3902fbad6
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2022-09-28 15:27:55 +0100

    updates to commandArgs + function wrapping

--- R/commandArgs.R
+++ R/commandArgs.R
@@ -2,20 +2,21 @@
 #' @title [base::commandArgs()] with Defaults
 #'
 #' @param trailingOnly see [base::commandArgs()];
-#' no effect on `args`
+#' no effect on `args`. NB: different default
 #'
-#' @param args character, result to return when
-#' debugging
+#' @param args character vector, result to return
+#' when `debugging == TRUE`
 #'
 #' @param debugging logical, use `args` or not
 #'
 #' @export
 commandArgs <- function(
-  trailingOnly,
-  args,
+  trailingOnly = TRUE,
+  args = c(),
   debugging = interactive()
 ) {
   if (debugging) {
+    if (length(args) == 0) warning("`args` is empty.")
     return(args)
   } else return(base::commandArgs(trailingOnly))
 }
@@ -26,30 +27,40 @@ commandArgs <- function(
 #' @description Provides a generic storage method.
 #'
 #' @param args character; where to store `obj`. Only
-#' the last item, i.e. `tail(args, 1)`, is used, so
+#' the last item, i.e. `tail(args, 1)`, is used, so e.g.
 #' all of the script arguments can be tossed in here if
 #' they end in the file to write to for output.
-#' Method of storage inferred from file extension.
+#' Method of storage inferred from file extension (not
+#' case-sensitive):
+#'  * png, jpg, tif(f), bmp: [ggplot2::ggsave()]
+#'  * rds: [saveRDS()]
+#'  * rda, rdata: [save()]
+#'  * csv: [data.table::fwrite()]
+#'  * txt: [writeLines()]
+#'  * other: Error message
 #'
 #' @param obj the thing to store; CAVEAT EMPTOR: no
 #' checking to see if `obj` is reasonable to store in
-#' `tail(args, 1)`.
+#' format inferred from `tail(args, 1)`.
 #'
-#' @param ... Other arguments to the inferred methods. See:
-#' [saveRDS()] for .rds, [save()] for .rda, [ggplot2::ggsave()]
-#' for image formats, [data.table::fwrite()] for csv
+#' @param ... Other arguments to the inferred methods
+#' (see `args` for relevant methods)
+#'
+#' @return whatever is returned from the inferred
+#' storage method.
 #'
 #' @export
 store <- function(args, obj, ...) {
   tarfile <- tail(args, 1)
   fun <- switch(
     tolower(tools::file_ext(tarfile)),
-    rds = \(fl, obj, ...) saveRDS(obj, file = fl, ...),
-    rda =, rdata = \(fl, obj, ...) save(obj, file = tarfile, ...),
+    rds = \(fl, obj, ...) base::saveRDS(obj, file = fl, ...),
+    rda =, rdata = \(fl, obj, ...) base::save(obj, file = tarfile, ...),
     png =, jpg =, tiff =, tif =,
     bmp = \(fl, obj, ...) ggplot2::ggsave(filename = fl, plot = obj, ...),
     csv = \(fl, obj, ...) data.table::fwrite(obj, file = fl, ...),
-    \(fl, ...) stop("didn't understand ", fl)
+    txt = \(fl, obj, ...) base::writeLines(text = obj, con = fl, ...),
+    \(fl, ...) stop("didn't understand format: ", fl)
   )
   return(fun(tarfile, obj, ...))
 }
---
+++ R/rejig.R
@@ -0,0 +1,158 @@
+#' @title [base::match.call] Extension
+#'
+#' @description Produces a `match.call` with defaults
+#' also included.
+#'
+#' @inheritParams base::match.call
+#'
+#' @export
+match.call.defaults <- function(
+  definition = sys.function(sys.parent()),
+  call = sys.call(sys.parent()),
+  expand.dots = TRUE,
+  envir = parent.frame(2L)
+) {
+  # get the call
+  mc <- match.call(definition, call, expand.dots, envir)
+  # get the formals, tossing any ellipsis
+  fs <- formals(definition, envir)
+  fs$... <- NULL
+
+  # for any arguments set in formals & not in the call
+  for(nm in setdiff(names(fs), names(mc)))
+    mc[[nm]] <- fs[[nm]] # add those to the call
+
+  return(mc)
+}
+
+#' Generic Function Wrapper
+#'
+#' @description provides a convenience function for
+#' producing duplicate functions with different
+#' defaults
+#'
+#' @param FUN the function to wrap
+#'
+#' @param ... the new defaults
+#'
+#' @param .ENV the environment for the resulting
+#' copy-function (i.e. where any variables will be
+#' evaluated). NB, the default (`environment(FUN)`) is
+#' mostly convenient, but can be dangerous e.g. by
+#' replacing an important function
+#'
+#'
+#' @return the new function
+#'
+#' @examples
+#' require(ggplot2); require(cabputils)
+#' require(data.table); require(deSolve)
+#'
+#' # make some reference plotting data
+#' dSIR <- function(t, SIR, params) with(params,{
+#'   infections <- beta*SIR[1]*SIR[2]
+#'   recoveries <- gamma*SIR[2]
+#'   list(c(-infections, infections-recoveries, recoveries))
+#' })
+#'
+#' dSEIR <- function(t, SEIR, params) with(params,{
+#'   infections <- beta*SEIR[1]*SEIR[3]
+#'   incubation <- lambda*SEIR[2]
+#'   recoveries <- gamma*SEIR[3]
+#'   list(c(-infections, infections-incubation, incubation-recoveries, recoveries))
+#' })
+#'
+#' SIR.dt <- ode(
+#'   c(S=100, I=1, R=0),
+#'   seq(0,50,by=0.1),
+#'   dSIR, parms = list(beta = 0.01, gamma = .2)
+#' ) |> as.data.table() |> melt(id.vars = "time")
+#'
+#' SEIR.dt <- ode(
+#'   c(S=100, E=0, I=1, R=0),
+#'   seq(0,50,by=0.1),
+#'   dSEIR, parms = list(beta = 0.01, lambda = .1, gamma = .2)
+#' ) |> as.data.table() |> melt(id.vars = "time")
+#'
+#' # USE rejig: define some scales to share
+#' scale_color_state <- rejig(
+#'   scale_color_manual,
+#'   name = paste0(breaks, collapse = ""),
+#'   values = c(S="dodgerblue", E="yellow", I="firebrick", R="limegreen")
+#' )
+#' scale_y_count <- rejig(scale_y_continuous, name = "Count")
+#'
+#' # note difference between these:
+#' scale_color_state
+#' scale_y_count
+#'
+#' scale_x_simtime <- rejig(scale_x_continuous, name = NULL)
+#'
+#' ggplot(SIR.dt) + aes(time, value, color = variable) +
+#'   geom_line() +
+#'   scale_color_state(breaks = levels(SIR.dt$variable)) +
+#'   scale_y_count() +
+#'   scale_x_simtime() +
+#'   theme_minimal()
+#'
+#' ggplot(SEIR.dt) + aes(time, value, color = variable) +
+#'   geom_line() +
+#'   scale_color_state(breaks = levels(SEIR.dt$variable)) +
+#'   scale_y_count() +
+#'   scale_x_simtime() +
+#'   theme_minimal()
+#'
+#' @export
+rejig <- function(FUN, ..., .ENV = environment(FUN)) {
+  # initial validation
+  stopifnot(
+    "FUN isn't a function." = is.function(FUN),
+    "FUN is a primitive function." = !is.primitive(FUN)
+  )
+
+  dots <- as.list(match.call())[-1] # get some new defaults
+  dots$FUN <- dots$.ENV <- NULL # drop all the not-defaults
+
+  if (length(dots) == 0) {
+    warning("... is empty. Just returning FUN.")
+    return(FUN)
+  }
+
+  .FUN <- FUN # make a duplicate of FUN
+  forms <- formals(FUN) # get the original defaults
+
+  # potentially more validation: check for ... argument
+  # in FUN and try to partial match all arguments in
+  # rejig
+  hasdots <- "..." %in% names(forms)
+  replacements <- names(forms)[pmatch(names(dots), names(forms))]
+
+  if (any(is.na(replacements)) && !hasdots) {
+    errmsg <- sprintf("
+FUN does not have ... argument, and
+rejig ... arguments do not match FUN arguments:
+%s
+", names(dots)[is.na(replacements)] |> paste(collapse = ", ")
+    )
+    stop(errmsg)
+  }
+
+  # correct any partially matched defaults
+  names(dots)[!is.na(replacements)] <- replacements[!is.na(replacements)]
+  # set the new defaults
+  formals(.FUN)[names(dots)] <- dots
+  environment(.FUN) <- .ENV
+
+  if (hasdots && any(is.na(replacements))) {
+    # the internals of FUN may pass around the ellipsis, which now
+    # excludes newly set default variables, so need to use it
+    body(.FUN) <- substitute({
+      mc <- cabputils::match.call.defaults()
+      mc[[1]] <- FUN
+      eval(mc)
+    })
+  }
+
+  return(.FUN)
+
+}
